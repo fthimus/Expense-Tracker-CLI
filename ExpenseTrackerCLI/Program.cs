5dcd5d0e07dbdab74c7e195a77818fb5c5f4e269
fthimus <fthimus@gmail.com>
2024-11-07 00:47:36 +0700

    Init boilerplate

---
+++ ExpenseTrackerCLI/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+    }
+
+    static void AddExpense()
+    {
+
+    }
+
+    static void UpdateExpense()
+    {
+
+    }
+
+    static void DeleteExpense()
+    {
+
+    }
+
+    static void ViewAllExpenses()
+    {
+
+    }
+
+    static void ViewSummaryAllExpenses()
+    {
+
+    }
+
+    static void ViewSummaryAllExpenses(int month)
+    {
+
+    }
+}
